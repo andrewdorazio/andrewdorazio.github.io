d8892350d333b14fb687a8b9abe9126f311c949e
AndrewDorazio <adorazio1997@gmail.com>
2022-03-23 17:41:14 -0400

    Import updated website

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Javascript file
 // Not sure what Im adding here yet
 
+
 let buttonFunction = () => {
     let hiddenButton = document.getElementById('hidden-neon-buttons')
+    console.log(hiddenButton.style.visibility)
     if (hiddenButton.style.visibility === 'hidden') {
         hiddenButton.style.visibility = 'visible';
     } else {
         hiddenButton.style.visibility = 'hidden';
     }
 }
+
